4e1b838859fd41c952174313f58ff2155481a16b
Artem622 <60893489+Artem622@users.noreply.github.com>
2022-09-14 18:15:32 +0300

    Add files via upload

---
+++ lb1.java
@@ -0,0 +1,43 @@
+import java.text.DecimalFormat;
+
+public class lb1 {
+    public static void main(String[] args) {
+
+        int par = 6;
+        int[] c = new int[7];
+
+        for (int i = 0; i < c.length; i++) {
+            c[i] = par;
+            par += 2;
+        }
+
+        float[] x = new float[13];
+        float max = 7.0F;
+        float min = -3.0F;
+
+        for (int i = 0; i < x.length; i++) {
+            double random = min + Math.random() * (max - min);
+            float randomius = (float) random;
+            x[i] = randomius;
+        }
+
+        double[][] t = new double[7][13];
+
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < x.length; j++) {
+                double X = x[j];
+                if (c[i] == 12) {
+                    t[i][j] = Math.log(Math.pow(Math.sin(Math.tan(Math.pow(X,1./3.))),2));
+                } else if (c[i] == 8 | c[i] == 14 | c[i] == 18) {
+                    t[i][j] = Math.log(Math.pow(((Math.pow(Math.sin(X),2) + 1) / Math.pow((3 / Math.abs(X) ), 2)) , Math.pow((((0.5 / (X + 1) - 3 / 4)) / 0.5),2)));
+                }else{
+                    t[i][j] = 1/4 * (Math.pow((Math.tan((Math.pow(X, (1/3)/X)))), ((Math.pow(((1/4)/(X - 1)*(Math.pow(Math.E,X))),Math.sin(X))) / 2)));
+                }
+                DecimalFormat df = new DecimalFormat("#.##");
+                System.out.print(df.format( t[i][j])+ "\t");
+            }
+            System.out.println();
+
+        }
+    }
+}
